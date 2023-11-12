6bcc608128572e0c1caa883761254b69625c82ef
OduogoWalter <oduogowalter@gmail.com>
2023-11-12 00:53:02 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,14 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
 
-
     # Remove the script name from arguments
     args = argv[1:]
 
-
     # Calculate the sum of all arguments
     result = sum(int(arg) for arg in args)
-
 
     # Print the result followed by a new line
     print(result)
