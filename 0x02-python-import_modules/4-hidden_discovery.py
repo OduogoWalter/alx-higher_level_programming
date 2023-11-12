1fb7a2c3b34abbe32acb8ecfdb317dfafbf87996
OduogoWalter <oduogowalter@gmail.com>
2023-11-12 01:26:45 -0800

    5-variable_load.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -7,7 +7,7 @@ def print_names():
     with open("hidden_4.pyc", "rb") as file:
         code = marshal.load(file)
 
-        names = [name for name in code.co_names oif not name.startswith('__')]
+        names = [name for name in code.co_names if not name.startswith('__')]
         names, sort()
 
         for name in names:
