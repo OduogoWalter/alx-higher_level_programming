b1a10efac3237136bb97571d62c7ca1b15afcdce
OduogoWalter <oduogowalter@gmail.com>
2023-11-11 23:32:48 -0800

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+
+    # Remove the script name from arguments
+    args = argv[1:]
+
+
+    # Calculate the sum of all arguments
+    result = sum(int(arg) for arg in args)
+
+
+    # Print the result followed by a new line
+    print(result)
