d7347fcf3b1cb1d9279179f1716c28d10a6da0c4
OduogoWalter <oduogowalter@gmail.com>
2023-11-13 06:24:41 -0800

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ''.join(char for char in my_string if char not in 'cC')
+    return new_string
+
+
+if __name__ == "__main__":
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
