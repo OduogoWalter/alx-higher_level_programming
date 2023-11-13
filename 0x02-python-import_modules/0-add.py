c5fd005ef9237eef3d78c82105ca50c938124409
OduogoWalter <oduogowalter@gmail.com>
2023-11-12 22:12:09 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    from add_0 import add
-    print("{} + {} = {}".format(a, b, add(a, b)))
+from add_0 import add
+a = 1
+b = 2
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
