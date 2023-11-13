d85b637ddf75c28b9ec2837afab519d5d81d6de7
OduogoWalter <oduogowalter@gmail.com>
2023-11-12 23:21:37 -0800

    2-args.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-from calculation_1 import add, sub, mul, div
+from calculator_1 import add, sub, mul, div
+
 a = 10
 b = 5
+
 res_add = add(a, b)
 res_sub = sub(a, b)
 res_mul = mul(a, b)
 res_div = div(a, b)
+
 print("{} + {} = {}".format(a, b, res_add))
 print("{} - {} = {}".format(a, b, res_sub))
 print("{} * {} = {}".format(a, b, res_mul))
