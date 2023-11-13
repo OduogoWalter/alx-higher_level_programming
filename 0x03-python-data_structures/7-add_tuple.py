1fda9a1cfb6f0c2a2a4a5c4b09730d2627ddb24a
OduogoWalter <oduogowalter@gmail.com>
2023-11-13 06:56:11 -0800

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple=()):
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+
+    sum_tuple = (a[0] + b[0], a[1] + b[1])
+
+    return sum_tuple
+
+
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
