53865350d98eec51b0c65e765a290343c79806d6
OduogoWalter <oduogowalter@gmail.com>
2023-11-15 05:32:11 -0800

    5-number_keys.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,5 @@
 
 def only_diff_elements(set_1, set_2):
     """Returns a set of all elements present in only one set."""
-    diff_set = set_1.symmetric_differences(set_2)
+    diff_set = set_1.symmetric_difference(set_2)
     return diff_set
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary."""
+    num_keys = len(a_dictionary)
+    return num_keys
