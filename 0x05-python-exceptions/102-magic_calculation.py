1c416f72f22e4e601b0440fb165f97e8973c3642
OduogoWalter <oduogowalter@gmail.com>
2023-11-20 04:38:18 -0800

    103-python.c

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,12 +2,12 @@
 
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise ValueError('Too far')
             result += (a ** b) / i
-        except ValueError as ve:
+        except ValueError:
             result = b + a
             break
     return (result)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,14 +3,13 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
-        for i in range(x):
-            if type(my_list[i], end=""):
+        while i < x:
+            if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
+            i += 1
     except TypeError:
         pass
-    finally:
-        print()
-        if count == x:
-            raise IndexError("list index out of range")
-        return (count)
+
+    print()
+    return (count)
