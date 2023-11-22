da57cd5e9ff3fae3a4195eb1971301ad1ea607a4
OduogoWalter <oduogowalter@gmail.com>
2023-11-21 22:47:28 -0800

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Square class witha private size attribute."""
+
+    def __init__(self, size=0):
+        """Initializes a new instance of the Square class."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method tyo retrieve the value of size."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set the value of the size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Computes the area of the square."""
+        return (self.__size ** 2)
