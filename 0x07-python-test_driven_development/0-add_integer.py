f4ae8c7c6ed00afe27a6e4e1f8dffa27639e7dd3
OduogoWalter <oduogowalter@gmail.com>
2023-12-11 20:05:20 -0800

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_integers(a, b=98):
+    """
+    Adds two intergers.
+
+
+    Arguments:
+    a (int or float): The first number.
+    b (int or float, optional): The second number. Defaults to 98.
+
+    Returns:
+    int: The sum of a and b.
+
+    Raises:
+    TypeError: If either a or b is not an integer or float.
+    """
