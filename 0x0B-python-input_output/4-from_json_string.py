b5dc127026491139c0fa8cab64b72ad5c5d23251
OduogoWalter <oduogowalter@gmail.com>
2023-12-24 14:22:03 +0300

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to return an object (Python data structure)
+represented by a JSON string."""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Return an object(Python data structure)
+    represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to be converted.
+
+    Returns:
+        object: The python data structure represented by the JSON string.
+    """
+    return (json.loads(my_str))
