8da405589eae5d5372919e72a71996aa48460df9
OduogoWalter <oduogowalter@gmail.com>
2023-12-31 15:20:18 +0300

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    # Check if idx is negative or out of range
+    """
+    Deletes the item at a specific position in a list.
+
+    Args:
+        my_list (list): The input list.
+        idx (int): The index of the item to be deleted.
+
+    Returns:
+        list: The modified list.
+    """
+    # Check if the index is negative or out of range
     if idx < 0 or idx >= len(my_list):
-        return my_list
+        return (my_list)
 
-    # Use slicing to delete the item at the specified index
-    new_list = my_list[:idx] + my_list[idx + 1:]
-    return new_list
+    # Create a new list excluding the item at the specified index
+    new_list = my_list[:idx] + my_list[idx+1:]
 
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_list = delete_at(my_list, idx)
-
-    print(new_list)
+    return (new_list)
