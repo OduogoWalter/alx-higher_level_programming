0249cabe9f61edcccbbe955b2572fe462ff9f396
OduogoWalter <oduogowalter@gmail.com>
2024-01-01 15:58:52 +0300

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,8 +5,11 @@ import sys
 def is_safe(board, row, col, n):
     """Check if it's safe to place a queen at board[row][col]"""
     for i in range(row):
-        if board[i] == col or
-        board[i] - i == col - row or board[i] + i == col + row:
+        if (
+            board[i] == col
+            or board[i] - i == col - row
+            or board[i] + i == col + row
+        ):
             return (False)
     return (True)
 
@@ -44,3 +47,16 @@ def nqueens(n):
     solutions = []
     solve_nqueens(board, 0, n, solutions)
     print_solutions(solutions)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} N".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        nqueens(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
