fce0bb9093742c45cfafb68b88f660e1378327db
OduogoWalter <oduogowalter@gmail.com>
2024-02-19 16:12:56 +0300

    pycodestyle 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,9 +2,11 @@
 """
 Module: 5-text_indentation
 
-This module defines a function text_indentation that prints a text with 2 new lines
+This module defines a function text_indentation
+that prints a text with 2 new lines
 after each occurrence of the characters '.', '?', and ':'.
 """
+
 
 def text_indentation(text):
     """
@@ -31,13 +33,18 @@ def text_indentation(text):
     for line in lines:
         print(line.strip())  # Remove leading and trailing whitespace
 
+
 if __name__ == "__main__":
-    text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-    Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-    Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+    text = """Lorem ipsum dolor sit amet, consecteturadipiscing elit. \
+    Quonam modo? Utrum igitur tibi
+    litteram videor an totas paginas commovere? \
+    Non autem hoc: igitur ne illud quidem. Fortasse
+    id optimum, sed ubi illud: \
     Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-    rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+    Transfer idem ad modestiam vel temperantiam, quae
+    est moderatio cupiditatum \
+    rationi oboediens. Si id dicis, vicimus. Inde
+    sermone vario sex illa a Dipylo \
     stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
     cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
     beatiorem! Iam ruinas videres"""
