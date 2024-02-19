ef14ff6c91d9ab1334cb67d63bd30b62320b7278
OduogoWalter <oduogowalter@gmail.com>
2024-02-19 08:53:54 +0300

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const myVar = "JavaScript is amazing";
-console.log(myVar);
+
+function callMeMoby(x, theFunction) {
+    for (let i = 0; i < x; i++) {
+      theFunction();
+    }
+  }
+  
+  module.exports = { callMeMoby };
+  
