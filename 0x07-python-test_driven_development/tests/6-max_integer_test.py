d0ad0ba34a1081a040a1c8a598b05a983b883227
OduogoWalter <oduogowalter@gmail.com>
2024-02-24 10:22:55 +0300

    test_driven_development

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([...])
+"""Unittest for max_integer([..])
 """
 import unittest
 from max_integer import max_integer
@@ -8,30 +8,39 @@ class TestMaxInteger(unittest.TestCase):
     def test_regular_list(self):
         """Test with a regular list"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_reverse_list(self):
+        """Test with a reverse list"""
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
+    def test_mixed_list(self):
+        """Test with a mixed list"""
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-    
+
     def test_empty_list(self):
         """Test with an empty list"""
         self.assertIsNone(max_integer([]))
 
-    def test_negative_numbers(self):
-        """Test with a list containing negative numbers"""
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-        self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
-
-    def test_mixed_numbers(self):
-        """Test with a list containing positive and negative numbers"""
-        self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
-        self.assertEqual(max_integer([-4, 3, -2, 1]), 3)
-
-    def test_single_element_list(self):
-        """Test with a single element list"""
+    def test_one_element_list(self):
+        """Test with a one-element list"""
         self.assertEqual(max_integer([7]), 7)
 
+    def test_negative_numbers(self):
+        """Test with a list of negative numbers"""
+        self.assertEqual(max_integer([-1, -5, -3, -10]), -1)
+
     def test_float_numbers(self):
-        """Test with a list containing float numbers"""
+        """Test with a list of float numbers"""
         self.assertEqual(max_integer([1.5, 2.5, 3.5]), 3.5)
-        self.assertEqual(max_integer([-1.5, -2.5, -3.5]), -1.5)
+
+    def test_duplicate_numbers(self):
+        """Test with a list containing duplicate numbers"""
+        self.assertEqual(max_integer([2, 2, 2]), 2)
+
+    def test_mixed_types(self):
+        """Test with a list containing mixed types"""
+        with self.assertRaises(TypeError):
+            max_integer([1, 'a', 3, 4])
 
 if __name__ == '__main__':
     unittest.main()
