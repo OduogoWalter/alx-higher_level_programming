e8f132ec429d3ab06e83326e7b833d8b65a30155
OduogoWalter <oduogowalter@gmail.com>
2024-02-28 12:49:23 +0300

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 
 import json
 
+
 class Base:
     """Base class for managing id attribute."""
 
@@ -15,10 +16,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
+
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON string representation of list_dictionaries."""
+        """Return the JSON string representation
+        of list_dictionaries."""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
