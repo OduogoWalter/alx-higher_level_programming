b03cdd40f446e6e47b5a7dbed0ef2963198b9266
OduogoWalter <oduogowalter@gmail.com>
2024-02-28 11:02:20 +0300

    almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,7 +5,7 @@
 class Base:
     """Base class for managing id attribute."""
 
-    __nb_objects = 0  # private class attribute
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """Initialize the Base instance with an optional id."""
@@ -14,20 +14,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-if __name__ == "__main__":
-    # Simple test script
-    b1 = Base()
-    print(b1.id)
-
-    b2 = Base()
-    print(b2.id)
-
-    b3 = Base()
-    print(b3.id)
-
-    b4 = Base(12)
-    print(b4.id)
-
-    b5 = Base()
-    print(b5.id)
