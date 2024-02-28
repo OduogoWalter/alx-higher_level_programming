2026f2a3c926d248d0889514c951b152d48a247d
OduogoWalter <oduogowalter@gmail.com>
2024-02-28 11:06:42 +0300

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,7 +22,12 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter for width attribute."""
-        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -32,7 +37,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for height attribute."""
-        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -42,7 +52,12 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter for x attribute."""
-        self.__x = value
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
@@ -52,16 +67,34 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter for y attribute."""
-        self.__y = value
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
 
 
 if __name__ == "__main__":
     # Test script
-    r1 = Rectangle(10, 2)
-    print(r1.id)
+    try:
+        Rectangle(10, "2")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
-    r2 = Rectangle(2, 10)
-    print(r2.id)
+    try:
+        r = Rectangle(10, 2)
+        r.width = -10
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
-    r3 = Rectangle(10, 2, 0, 0, 12)
-    print(r3.id)
+    try:
+        r = Rectangle(10, 2)
+        r.x = {}
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        Rectangle(10, 2, 3, -1)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
