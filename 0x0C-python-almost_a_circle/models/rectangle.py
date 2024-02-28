fdac711bb26210b8117ebc3b4ac3fe7639e1691e
OduogoWalter <oduogowalter@gmail.com>
2024-02-28 10:46:12 +0300

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Module for the Rectangle class."""
+from base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class inherits from Base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a Rectangle instance."""
+        super().__init__(id)  # Call superclass constructor with id
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width attribute."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height attribute."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x attribute."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y attribute."""
+        self.__y = value
+
+
+if __name__ == "__main__":
+    # Simple test script
+    r1 = Rectangle(10, 2)
+    print(r1.id)
+
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
