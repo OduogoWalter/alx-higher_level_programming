abc1578b48b142730302394df782c58c3738a790
OduogoWalter <oduogowalter@gmail.com>
2024-02-28 10:13:15 +0300

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Module with class MyList"""
-
-
 class MyList(list):
-    """A class that inherits from list"""
-
+    """
+    MyList class that inherits from list
+    """
     def print_sorted(self):
-        """Prints the list in ascending order"""
         print(sorted(self))
