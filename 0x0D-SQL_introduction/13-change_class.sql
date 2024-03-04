f5a7bab33360df92507fba1324bdceed797d25a0
OduogoWalter <oduogowalter@gmail.com>
2024-03-04 19:03:34 +0300

    13-change_class.sql

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,6 @@
--- Script to remove all records with a score <= 5 in the table second_table
+-- Script to remove all records with a
+-- score <= 5 in the table second_table
 
-DELETE FROM hbtn_0c_0.second_table
+DELETE FROM second_table
 WHERE score <= 5;
+
