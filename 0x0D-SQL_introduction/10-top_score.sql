3069c549c504a135b3d6c94b9a4c00bf0dbace83
OduogoWalter <oduogowalter@gmail.com>
2024-03-04 18:40:04 +0300

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,7 +1,7 @@
 -- lists all records of the table second_table
--- of the database hbtn_0c_0 in your MySQL server
+-- of the database hbtn_0c_0 in your MySQL server.
 
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM second_table
 ORDER BY score DESC;
 
