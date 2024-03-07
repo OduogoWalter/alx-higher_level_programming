b2596ea44d49ef661d4d98832a2d7b33b0cdbaa1
OduogoWalter <oduogowalter@gmail.com>
2024-03-07 23:32:08 +0300

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- 1-create_user.sql
+
+-- Create MySQL user user_0d_1 with all privileges and set password if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
