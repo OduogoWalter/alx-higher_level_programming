c8e3c0c6fea2eab2e48da67b33aa4e715474aa9e
OduogoWalter <oduogowalter@gmail.com>
2024-03-25 07:59:57 +0300

    python-network_0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL -w "%{http_code}" "$1" -o /dev/null | grep "200" &>/dev/null && curl -sL "$1"
-
