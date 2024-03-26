3e3b520cbc6d4c83bfca316cb1fde762adb33924
OduogoWalter <oduogowalter@gmail.com>
2024-03-26 11:28:30 +0300

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Fetches a URL using urllib package
+"""
+
+from urllib import request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
+
