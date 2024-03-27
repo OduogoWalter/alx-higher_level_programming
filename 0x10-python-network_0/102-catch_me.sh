cece65350476e1e4ce6b81b4667df4f22fabdc5b
OduogoWalter <oduogowalter@gmail.com>
2024-03-27 14:08:43 +0300

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me
-curl -sX PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+# Script to send a request to 0.0.0.0:5000/catch_me that makes the server respond with "You got me!"
+curl -sX PUT 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool" -d "user_id=98" -L
 
