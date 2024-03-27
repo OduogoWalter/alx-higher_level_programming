c03eac6fda987f9c8f0094b5e812ee06fa203e5d
OduogoWalter <oduogowalter@gmail.com>
2024-03-27 13:01:49 +0300

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-# This script sends a JSON POST request to a URL passed as the first argument, with the contents of a file passed as the second argument
+# This script sends a JSON POST request to a URL.
+# First argument: URL to send the request to.
+# Second argument: Filename of the file containing JSON data to send.
 
-# Check if the file exists
-if [ ! -f "$2" ]; then
-    echo "File not found: $2"
-    exit 1
-fi
+url="$1"
+json_file="$2"
 
-# Check if the file contains valid JSON
-if ! jq -e . >/dev/null 2>&1 <"$2"; then
-    echo "Not a valid JSON"
-    exit 1
-fi
+curl -s -X POST -H "Content-Type: application/json" -d @"$json_file" "$url"
 
-# Send the POST request with the JSON content
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
