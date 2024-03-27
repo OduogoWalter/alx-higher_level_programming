2df85be1c318b358a90fa4bc39278bc422b57c34
OduogoWalter <oduogowalter@gmail.com>
2024-03-27 14:16:22 +0300

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a URL passed as the first argument,
-# with the contents of a file (second argument) as the request body,
-# and displays the response body.
-
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 URL FILE"
-    exit 1
-fi
-
-URL=$1
-FILE=$2
-
-curl -sX POST -H "Content-Type: application/json" -d @"$FILE" "$URL"
+# Sends a JSON POST request to a URL and displays the body of the response
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
